910a2fd34a96e4e5f9424a6d55a74fcc5a5d5dea
lplaaza <l.plazaa.2020@alumnos.urjc.es>
2025-05-13 18:13:22 +0200

    Mejoras css

--- P3/public/chat.js
+++ P3/public/chat.js
@@ -11,6 +11,7 @@ socket.emit('set nickname', nickname);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     if (input.value) {
+        addMessage(`💬 ${nickname}: ${input.value}`, true); // Mostramos localmente
         socket.emit('chat message', input.value);
         input.value = '';
     }
@@ -21,12 +22,8 @@ input.addEventListener('input', () => {
 });
 
 socket.on('chat message', (msg) => {
-    const item = document.createElement('li');
-    item.textContent = msg;
-    messages.appendChild(item);
-    window.scrollTo(0, document.body.scrollHeight);
-    
     if (!msg.includes(nickname)) {
+        addMessage(msg, false);
         notifySound.play();
     }
 });
@@ -35,3 +32,12 @@ socket.on('typing', (user) => {
     typing.textContent = user;
     setTimeout(() => typing.textContent = '', 2000);
 });
+
+function addMessage(msg, isSelf) {
+    const item = document.createElement('li');
+    item.classList.add('message');
+    item.classList.add(isSelf ? 'self' : 'other');
+    item.textContent = msg;
+    messages.appendChild(item);
+    messages.scrollTop = messages.scrollHeight;
+}
